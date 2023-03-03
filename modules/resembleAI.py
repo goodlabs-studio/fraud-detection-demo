1e39f7dd7051567caeb586cd376f43c75bf2d73e
Paul Gege <paul@Pauls-MacBook-Pro.local>
2023-03-03 11:05:14 -0500

    clear resemble ai input before speaking

--- modules/resembleAI.py
+++ modules/resembleAI.py
@@ -21,11 +21,8 @@ def text_to_speech(driver, By, EC, WebDriverWait, Keys, project, clip, paragraph
         EC.presence_of_element_located((By.XPATH, '//*[@data-slate-leaf="true"]'))
     )
     
-    # trying to clear the input before typing
-    # word_input.send_keys(u'\ue009' + u'\ue003')
-    word_input.click()
-    word_input.send_keys(Keys.CONTROL + "a")
-    word_input.send_keys(Keys.BACKSPACE)
+    # change to Keys.CONTROL if not on mac
+    word_input.send_keys(Keys.COMMAND, "a")
     word_input.send_keys(paragraph)
 
     # driver.switch_to.active_element.send_keys("TEST")
